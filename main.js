4b7fdb0ffa3df94ea420c34db137365dc900a9c7
philwelsh <Phizs@hotmail.com>
2014-10-29 12:13:42 +0000

    svg wait to load

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
 ﻿
 $(function () {
-    //slick.js
+    //Delay transitions until page loaded
+    //for title chalk animations
+    $("window").load(function () {
+        $("body").removeClass("preload");
+    });
+
+    //slick.js 
+    //for experience slider
     $('.single-item').slick();
 
-    // Init Skrollr
-    //if (window.innerWidth > 1000) {
-    //    var s = skrollr.init({
-    //        render: function (data) {
-    //            //Debugging - Log the current scroll position.
-    //            //console.log(data.curTop);
-    //        }
-    //    });
-    //};
     //SCROLL TO
     $("#topmenu li a.navbutton").on("click", function () {
         var destination = $(this).attr('data-attr-scroll')
         $('body').scrollTo(destination, { duration: 'slow' });
     });
     $("#returntotop").on("click", function () {
-        //$('body').scrollTo("#topmenu", { duration: 'slow' });
         $('body').scrollTo("body", { duration: 'slow' });
     });
-
 
     //SCROLL EFFECTS
     /* Every time the window is scrolled ... */
@@ -38,7 +34,7 @@ $(function () {
         //APPEAR ON SCROLL
 
         /* Check the location of each desired element */
-        $('.hideme').each(function (i) {
+        $('.hideme').each(function () {
 
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
             var top_of_object = $(this).position().top;
@@ -46,7 +42,7 @@ $(function () {
 
             /* If the object is completely visible in the window, fade it in */
             if (bottom_of_window > top_of_object) {
-
+                //$(this).removeClass("hideme");
                 $(this).delay(200).animate({ 'opacity': '1' }, 800);
                 $(this).find(".nth1 .filler").delay(400).animate({ 'height': '100%' }, 1300);
                 $(this).find(".nth2 .filler").delay(500).animate({ 'height': '100%' }, 1350);
@@ -60,15 +56,16 @@ $(function () {
                 $(this).find(".nth5 .percent").delay(900).animate({ 'top': '10%' }, 1800);
             }
             //Change to '.addclass("animate") then add that to CSS
+            //$(this).delay(200).animate({ 'opacity': '1' }, 800);
 
 
         });
-    }, 750));
+    }, 750));//throttle amount//
 
     //Skill buttons
 
     var currentview = "#skillsnth0";
-    var lock = false;
+    var lock = false;   //lock prevents multiple tabs opening
     $("#skillbuttons a").on("click", function () {
         if (lock) return;
         lock = true;
@@ -80,9 +77,4 @@ $(function () {
             lock = false;
         });
     });
-
-
-
-
-
 });
